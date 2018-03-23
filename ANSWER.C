5fd49cc6fa5e81aca03f7a6e977f766ce5e14c39
janakisaras <35526104+janakisaras@users.noreply.github.com>
2018-03-23 16:31:02 +0530

    ANSWER.C
    THE PROGRAM TO PRINT THE ANSWER

---
+++ ANSWER.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int A,B,C;
+            clrscr();
+	printf("\nEnter the number A,B,C");
+	scanf("%d%d%d",&A,&B,&C);
+            A=(A*B)/C;
+	printf("The answer is:%d",a);
+	getch();
+}
